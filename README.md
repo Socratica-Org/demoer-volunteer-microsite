8b01eae2a53bf9a5d59516ace4fe647138c82d8c
sohambasu963 <sohambasu963@gmail.com>
2025-02-21 20:45:13 -0500

    created next app

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # demoer-volunteer-microsite
+
 A microsite to invite and thank demoers and volunteers for the W25 Socratica Symposium
---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Socratica Symposium",
+  description: "Thank you for your participation in the Socratica Symposium",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
